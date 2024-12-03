d582606eaae7a91b57b36a9eb8258999299158e6
t.iskakov <lightx_design@yahoo.com>
2024-12-03 23:42:33 +0600

    add interaction test

---
+++ tests/integration/domInteraction.test.ts
@@ -0,0 +1,17 @@
+import { SimpleInputMask } from "../../src/simpleInputMask";
+
+test("Input updates correctly with mask", () => {
+
+    const input = document.createElement("input");
+
+    const mask = new SimpleInputMask({ mask: "(###) ###-####" });
+
+    mask.attach(input);
+
+    input.value = "1234567890";
+    input.dispatchEvent(new Event("input"));
+
+    expect(input.value).toBe("(123) 456-7890");
+
+    mask.detach();
+});
